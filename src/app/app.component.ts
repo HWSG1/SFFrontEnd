57ba20ae9b3b7acc60fd544ddd075f24db08fab9
HWSG1 <odairumanzor@gmail.com>
2025-01-01 06:08:15 -0600

    carga de login html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./auth/login/login.component";
+import { CommonModule } from '@angular/common'; // Importa CommonModule
+
 
 @Component({
   selector: 'app-root',
-  //imports: [RouterOutlet],
+  imports: [LoginComponent, CommonModule], //RouterOutlet
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  showPassword = false;
 
+  togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
+  }
 }
